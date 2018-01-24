ca652b317f3b0bf55b12db2b1de3501c9e2fe1fe
Whitney Armstrong <warmstrong@anl.gov>
2018-01-24 17:55:08 -0500

    modified:   bubble_chamber.cc

--- bubble_chamber.cc
+++ bubble_chamber.cc
@@ -2,9 +2,10 @@
 #include "B1DetectorConstruction.hh"
 #include "B1ActionInitialization.hh"
 #include "G4SystemOfUnits.hh"
-#include "getopt.h"
 
 #include <cstdio>
+#include <iostream>
+#include "clipp.h"
 
 #ifdef G4MULTITHREADED
 #include "G4MTRunManager.hh"
@@ -34,122 +35,151 @@ bool fexists(const std::string& filename) {
 }
 //______________________________________________________________________________
 
-void print_help() {
-
-   std::cout << "usage: bubble_chamber [options] [macro file]    \n";
-   std::cout << "Options:                               \n";
-   std::cout << "    --run=#, -r         set file \"run\" number\n";
-   std::cout << "    --gui=#, -g         set to 1 (default) to use qt gui or\n";
-   std::cout << "                        0 to use command line\n";
-   std::cout << "    --vis=#, -V         set to 1 (default) to visualization geometry and events\n";
-   std::cout << "                        0 to turn off visualization\n";
-   std::cout << "    --interactive, -i   run in interactive mode (default)\n";
-   std::cout << "    --batch, -b         run in batch mode\n"; 
+template<class C>
+void print_help(C cli)
+{
+  std::cout << make_man_page(cli, "bubble_chamber") << "\n";
+  //std::cout << "usage: bubble_chamber [options] [macro file]    \n";
+  //std::cout << "Options:                               \n";
+  //std::cout << "    --run=#, -r         set file \"run\" number\n";
+  //std::cout << "    --gui=#, -g         set to 1 (default) to use qt gui or\n";
+  //std::cout << "                        0 to use command line\n";
+  //std::cout << "    --vis=#, -V         set to 1 (default) to visualization geometry and events\n";
+  //std::cout << "                        0 to turn off visualization\n";
+  //std::cout << "    --interactive, -i   run in interactive mode (default)\n";
+  //std::cout << "    --batch, -b         run in batch mode\n"; 
 }
 
 //______________________________________________________________________________
 
-int main(int argc,char** argv)
-{
-
+struct settings {
   int          run_number        = 0;
   int          number_of_events  = -1;
   std::string  output_file_name  = "";
   std::string  output_tree_name  = "";
   std::string  theRest           = "";
+  std::string  macroFile         = "";
   bool         run_manager_init  = false;
   bool         use_gui           = true;
   bool         use_vis           = true;
   bool         is_interactive    = true;
   bool         has_macro_file    = false;
+  bool         help              = false;
+};
+
+int main(int argc,char** argv)
+{
+  using namespace clipp;
+  using std::cout;
+  settings S;
+
+  auto cli = (
+    (option("-r", "--run") & value("run_number",S.run_number))      % "set the run number",
+    option("-b", "--batch")([&](){S.use_gui=false; S.use_vis=false; S.is_interactive=false;}) % "run in batch mode (no gui or vis)",
+    (option("-g", "--gui") & integer("gui")([&](auto v){S.use_gui=v;}))                 % "use GUI ",
+    option("-i", "--interactive")([&](){S.is_interactive=true;}) % "run in interactivemode ",
+    (option("-v", "--vis") & integer("vis")([&](auto v){S.use_vis=v;})) % "use vis ",
+    option("-h", "--help").set(S.help,true)
+    );
+
+  if(!parse(argc, argv, cli)) {
+    cout << make_man_page(cli, argv[0]);
+    return 0;
+  }
+
+  if(S.help) {
+    print_help(cli);
+    return 0;
+  }
+
 
   //---------------------------------------------------------------------------
 
-  int index = 0;
-  int iarg  = 0;
-  opterr    = 1; //turn off getopt error message
-  const struct option longopts[] =
-  {
-    {"run",         required_argument,  0, 'r'},
-    {"gui",         required_argument,  0, 'g'},
-    {"vis",         required_argument,  0, 'V'},
-    {"interactive", no_argument,        0, 'i'},
-    {"batch",       no_argument,        0, 'b'},
-    {"output",      required_argument,  0, 'o'},
-    {"tree",        required_argument,  0, 't'},
-    {"help",        no_argument,        0, 'h'},
-    {"init",        no_argument,        0, 'I'},
-    {0,0,0,0}
-  };
-  while(iarg != -1) {
-    iarg = getopt_long(argc, argv, "o:h:g:r:V:ibhI", longopts, &index);
+  //int index = 0;
+  //int iarg  = 0;
+  //opterr    = 1; //turn off getopt error message
+  //const struct option longopts[] =
+  //{
+  //  {"run",         required_argument,  0, 'r'},
+  //  {"gui",         required_argument,  0, 'g'},
+  //  {"vis",         required_argument,  0, 'V'},
+  //  {"interactive", no_argument,        0, 'i'},
+  //  {"batch",       no_argument,        0, 'b'},
+  //  {"output",      required_argument,  0, 'o'},
+  //  {"tree",        required_argument,  0, 't'},
+  //  {"help",        no_argument,        0, 'h'},
+  //  {"init",        no_argument,        0, 'I'},
+  //  {0,0,0,0}
+  //};
+  //while(iarg != -1) {
+  //  iarg = getopt_long(argc, argv, "o:h:g:r:V:ibhI", longopts, &index);
 
-    switch (iarg)
-    {
-      case 'b':
-        is_interactive = false;
-        use_gui = false;
-        use_vis = false;
-        break;
+  //  switch (iarg)
+  //  {
+  //    case 'b':
+  //      is_interactive = false;
+  //      use_gui = false;
+  //      use_vis = false;
+  //      break;
 
-      case 'i':
-        is_interactive = true;
-        break;
+  //    case 'i':
+  //      is_interactive = true;
+  //      break;
 
-      case 'V':
-        if( atoi(optarg) == 0 ){
-          use_vis = false;
-        } else  {
-          use_vis = true;
-        }
-        break;
+  //    case 'V':
+  //      if( atoi(optarg) == 0 ){
+  //        use_vis = false;
+  //      } else  {
+  //        use_vis = true;
+  //      }
+  //      break;
 
-      case 'r':
-        run_number = atoi( optarg );
-        break;
+  //    case 'r':
+  //      run_number = atoi( optarg );
+  //      break;
 
-      case 'g':
-        if( atoi(optarg) == 0 ){
-          use_gui = false;
-        } else  {
-          use_gui = true;
-        }
-        break;
+  //    case 'g':
+  //      if( atoi(optarg) == 0 ){
+  //        use_gui = false;
+  //      } else  {
+  //        use_gui = true;
+  //      }
+  //      break;
 
-      case 't':
-        output_tree_name = optarg;
-        break;
+  //    case 't':
+  //      output_tree_name = optarg;
+  //      break;
 
-      case 'I':
-        run_manager_init = true;
-        break;
+  //    case 'I':
+  //      run_manager_init = true;
+  //      break;
 
-      case 'o':
-        output_file_name = optarg;
-        if( fexists(output_file_name) ) {
-          std::cout << "Error : " << output_file_name << " already exist"  << std::endl;
-          exit(EXIT_FAILURE);
-        }
-        break;
+  //    case 'o':
+  //      output_file_name = optarg;
+  //      if( fexists(output_file_name) ) {
+  //        std::cout << "Error : " << output_file_name << " already exist"  << std::endl;
+  //        exit(EXIT_FAILURE);
+  //      }
+  //      break;
 
-      case 'h':
-        print_help();
-        exit(0);
-        break;
+  //    case 'h':
+  //      print_help();
+  //      exit(0);
+  //      break;
 
-      case '?':
-        print_help();
-        exit(EXIT_FAILURE);
-        break;
-    }
-  }
+  //    case '?':
+  //      print_help();
+  //      exit(EXIT_FAILURE);
+  //      break;
+  //  }
+  //}
 
   // here we assume the last argument is a macro file 
   if( optind < argc ) {
-    has_macro_file = true;
+    S.has_macro_file = true;
   }
   for (int i = optind; i < argc; i++) {
-    theRest        += argv[i];
+    S.theRest        += argv[i];
   }
 
   // Get the piped commands
@@ -162,17 +192,17 @@ int main(int argc,char** argv)
     }
   }
 
-  std::cout << " the rest of the arguments: " << theRest << std::endl;
-  std::cout << "output : " << output_file_name << std::endl;
-  std::cout << "  tree : " << output_tree_name << std::endl;
+  std::cout << " the rest of the arguments: " << S.theRest << std::endl;
+  std::cout << "output : " << S.output_file_name << std::endl;
+  std::cout << "  tree : " << S.output_tree_name << std::endl;
 
   //---------------------------------------------------------------------------
 
   // Detect interactive mode (if no arguments) and define UI session
   // Note third argument of G4UIExecutive can be ("qt", "xm", "win32", "gag", "tcsh", "csh")
   G4UIExecutive* ui = 0;
-  if( is_interactive || use_gui ) {
-    if( use_gui ) {
+  if( S.is_interactive || S.use_gui ) {
+    if( S.use_gui ) {
       ui = new G4UIExecutive(argc, argv, "qt");
     } else {
       ui = new G4UIExecutive(argc, argv, "tcsh");
@@ -232,7 +262,7 @@ int main(int argc,char** argv)
   runManager->SetUserInitialization(physicsList);
 
   // User action initialization
-  runManager->SetUserInitialization(new B1ActionInitialization(run_number));
+  runManager->SetUserInitialization(new B1ActionInitialization(S.run_number));
 
   // Initialize G4 kernel
   runManager->Initialize();
@@ -253,24 +283,26 @@ int main(int argc,char** argv)
 
   // Process macro or start UI session
 
-  // batch mode
-  if( has_macro_file ) {
+  if( S.has_macro_file ) {
+    
     G4String command = "/control/execute ";
-    G4String fileName = argv[optind];
+    G4String fileName = S.macroFile;
     UImanager->ApplyCommand(command+fileName);
+
   } else {
 
+    // run init
     // interactive mode
     G4String command   = "/control/macroPath ";
     G4String mac_dir   = BUBBLESIM_MACRO_DIR;
-    G4String fileName = "init_default.mac";
+    G4String fileName  = "init_default.mac";
 
     std::cout << " executing " << command+mac_dir << std::endl;
     UImanager->ApplyCommand(command+mac_dir);
 
     command = "/control/execute ";
 
-    if( use_vis ) { 
+    if( S.use_vis ) { 
       std::cout << " executing " << command+fileName << std::endl;
       UImanager->ApplyCommand(command+fileName);
     }
@@ -282,14 +314,13 @@ int main(int argc,char** argv)
     UImanager->ApplyCommand(G4String(cmd));
   }
 
-
   // run the set number of events
-  if( number_of_events > 0) {
-    G4String command = "/run/beamOn " + std::to_string(number_of_events);
+  if( S.number_of_events > 0) {
+    G4String command = "/run/beamOn " + std::to_string(S.number_of_events);
     UImanager->ApplyCommand( command );
   }
 
-  if( is_interactive )  {
+  if( S.is_interactive )  {
     ui->SessionStart();
     delete ui;
   }
